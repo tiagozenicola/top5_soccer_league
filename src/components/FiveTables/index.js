12243ef4a090b80ad2b140aa481cdf5b62627596
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2018-12-11 16:17:10 -0200

    Get teams from table

--- src/components/FiveTables/index.js
+++ src/components/FiveTables/index.js
@@ -8,8 +8,42 @@ class FiveTables extends Component {
     const html = this.getAsHtml(data)
     const tables = html.getElementsByTagName('table')
     const majorTables = Array.from(tables).slice(0,5)
-    console.log(majorTables)
+    console.log(majorTables);
+    console.log(this.getTeamsFromTable(majorTables[0]))
+    console.log(this.getTeamsFromTable(majorTables[1]))
+    console.log(this.getTeamsFromTable(majorTables[2]))
+    console.log(this.getTeamsFromTable(majorTables[3]))
+    console.log(this.getTeamsFromTable(majorTables[4]))
     return {teams: html};
+  }
+
+  getTeamsFromTable = table => {
+
+    const teams = []
+
+    table.querySelectorAll('tr').forEach((row, index) => {
+
+      if (index === 0 || index === 5){
+        return
+      }
+
+      const columns = row.querySelectorAll('td')
+
+      teams.push({
+        "name": columns[1].innerText,
+        "games_played": columns[2].innerText,
+        "win": columns[3].innerText,
+        "drawn": columns[4].innerText,
+        "lost": columns[5].innerText,
+        "goals_for": columns[6].innerText,
+        "goals_against": columns[7].innerText,
+        "goal_difference": columns[8].innerText,
+        "points": columns[9].innerText,
+        "history": columns[10].innerText,
+      })
+
+    })
+    return teams
   }
 
   getAsHtml = text => {
