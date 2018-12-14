76af1812b4edffc6c80ab4472ba62491222c3a04
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2018-12-14 00:38:11 -0200

    Split screen into soccer, NBA and NFL

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,26 @@
 import React, { Component } from 'react';
 import '../App.css';
-import Table from './Table/table.js';
+import FiveTables from './FiveTables';
 
 class App extends Component {
 
   render() {
-    const teams = [
-      {
-          "name": "Man City",
-          "games_played": 13,
-          "win": 11,
-          "drawn": 2,
-          "lost": 0,
-          "goals_for": 40,
-          "goals_against": 5,
-          "goal_difference": 35,
-          "points": 35,
-          "history": ['w','w','w','w','w'],
-      },
-      {
-          "name": "Barcelona",
-          "games_played": 13,
-          "win": 7,
-          "drawn": 4,
-          "lost": 2,
-          "goals_for": 35,
-          "goals_against": 19,
-          "goal_difference": 16,
-          "points": 25,
-          "history": ['w','w','w','l','d'],
-      },
-    ];
+    console.log('render', this)
+    const {screen} = this.state || {};
+    console.log('render2', screen)
 
     return (
       <div className="App">
-        <Table teams={teams}/>
+        <button onClick={() => this.setState({screen: 'soccer'})}>
+          Soccer
+        </button>
+        <button onClick={() => this.setState({screen: 'nba'})}>
+          NBA
+        </button>
+        <button onClick={() => this.setState({screen: 'nfl'})}>
+          NFL
+        </button>
+        {screen === 'soccer' && <FiveTables />}
       </div>
     );
   }
