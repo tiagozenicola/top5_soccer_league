e328746b328893daa35148b69da6423da3e04543
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2018-12-30 21:27:01 -0200

    Fix lint automatically

--- src/tests/Link.test.jsx
+++ src/tests/Link.test.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import enzyme, { mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import Link from '../components/atoms/Link'
-import 'jest-styled-components'
+import React from 'react';
+import renderer from 'react-test-renderer';
+import enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ButtonLink from '../components/atoms/Link';
+import 'jest-styled-components';
 
-enzyme.configure({ adapter: new Adapter() })
+enzyme.configure({ adapter: new Adapter() });
 
 it('renders Link correctly', () => {
-  const link = renderer.create(<Link />).toJSON()
-  expect(link).toMatchSnapshot()
-})
+  const link = renderer.create(<ButtonLink />).toJSON();
+  expect(link).toMatchSnapshot();
+});
