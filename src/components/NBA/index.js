3a837520b9c7dc2c296cd9a07dfbf3f85d7b9773
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2018-12-30 00:26:10 -0200

    Refactor component as a pure function

--- src/components/NBA/index.js
+++ src/components/NBA/index.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from './style';
 
-class NBA extends Component {
-  render() {
-    return (
-      <Container className="App">
-        NBA goes here
-      </Container>
-    );
-  }
+const NBA = (props) => {
+  return (
+    <Container className="App">
+      NBA goes here
+    </Container>
+  );
 }
 
 export default NBA;
--- src/components/NFL/index.js
+++ src/components/NFL/index.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from './style';
 
-class NFL extends Component {
-  render() {
-    return (
-      <Container>
-        NFL goes here
-      </Container>
-    );
-  }
+const NFL = () => {
+  return (
+    <Container>
+      NFL goes here
+    </Container>
+  );
 }
 
 export default NFL;
