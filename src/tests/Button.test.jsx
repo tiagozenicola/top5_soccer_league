2dd16bb7d4fd352b01cf35d079a47153a0603130
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2018-12-30 21:23:26 -0200

    Test onClick function

--- src/tests/Button.test.jsx
+++ src/tests/Button.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import enzyme, { mount } from 'enzyme'
+import enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Button from '../components/Button'
 import 'jest-styled-components'
+import { wrap } from 'module';
 
 enzyme.configure({ adapter: new Adapter() })
 
@@ -23,4 +24,11 @@ it('renders Button sorting desc', () => {
   const sort = () => {}
   const button = renderer.create(<Button value="P" name="position" sortProperty="position" orientation={1} onClick={sort}/>).toJSON()
   expect(button).toMatchSnapshot()
+})
+
+it('test that click calls my function', () => {
+  const sort = jest.fn();
+  const button = shallow(<Button value="P" name="position" sortProperty="position" orientation={1} onClick={sort}/>)
+  button.simulate("click");
+  expect(sort).toHaveBeenCalledTimes(1);
 })
