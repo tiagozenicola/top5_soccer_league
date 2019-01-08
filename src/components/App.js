d62d6705b5a6c091b8e2629f5b0dd161d1ed2d72
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2019-01-08 00:43:01 -0200

    Should show 'This should not be here 6'

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           Soccer
         </button>
         <button type="button" onClick={() => this.setState({ screen: 'nba' })}>
-          This should not be here 5
+          This should not be here 6
         </button>
         <button type="button" onClick={() => this.setState({ screen: 'nfl' })}>
           NFL
